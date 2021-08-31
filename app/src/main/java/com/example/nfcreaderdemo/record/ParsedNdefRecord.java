b14dd9bbf7497a9bea6cc5952fd17f8c897c785c
Andrea Bonatti <bonand@ideabs.local>
2021-08-31 09:43:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/nfcreaderdemo/record/ParsedNdefRecord.java
@@ -0,0 +1,6 @@
+package com.example.nfcreaderdemo.record;
+
+// Interfaccia per implementare i diversi tipi di dati da poter leggere con l'NFC
+public interface ParsedNdefRecord {
+    String str();
+}
